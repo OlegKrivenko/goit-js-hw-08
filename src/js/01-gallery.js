1684beff9334f62cecd04f483af576098243a219
OlegKrivenko <krivenko.olegus@gmail.com>
2023-02-28 17:33:09 +0200

    done hw 1 and 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,29 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// Подключение библиотеки SimpleLightbox (1-й импорт)
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей библиотеки SimpleLightbox (2-й импорт)
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const ref = {
+  gallery: document.querySelector('.gallery'),
+};
+
+ref.gallery.insertAdjacentHTML('beforeend', createGalleryItems(galleryItems));
+
+function createGalleryItems(arrayGalleryItems) {
+  return arrayGalleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+              </a>`;
+    })
+    .join('');
+}
+
+//      модальне вікно з великою фоткою SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
+
+const currentTime = function (data) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+
+  let time = Number(localStorage.getItem('videoplayer-current-time'));
+  console.log(time);
+};
+
+player.on('timeupdate', throttle(currentTime, 1000));
+
+player
+  .setCurrentTime(Number(localStorage.getItem('videoplayer-current-time')))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
